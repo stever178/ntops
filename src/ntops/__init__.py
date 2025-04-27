d794561e3ca7dd7b341d32967a2bbb73eae4fe04
Zimin Li <coollizimin@gmail.com>
2025-04-27 21:17:03 +0800

    Add `rsqrt` operator

--- src/ntops/__init__.py
+++ src/ntops/__init__.py
@@ -7,5 +7,6 @@ from ntops.exp import exp
 from ntops.gelu import gelu
 from ntops.mm import mm
 from ntops.mul import mul
+from ntops.rsqrt import rsqrt
 
-__all__ = ["abs", "add", "addmm", "bmm", "div", "exp", "gelu", "mm", "mul"]
+__all__ = ["abs", "add", "addmm", "bmm", "div", "exp", "gelu", "mm", "mul", "rsqrt"]
