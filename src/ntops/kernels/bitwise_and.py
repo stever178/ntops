2ee5df37b489c8527aeb77e6e2b59321118dfa09
stever178 <2874146120@qq.com>
2025-05-16 13:05:12 -0400

    Add `bitwise_and` operator

---
+++ src/ntops/kernels/bitwise_and.py
@@ -0,0 +1,25 @@
+import functools
+
+import ninetoothed
+import ninetoothed.language as ntl
+from ninetoothed import Tensor
+
+from ntops.kernels.element_wise import arrangement
+
+
+def application(input, other, output):
+    output = ntl.inline_asm_elementwise(  # noqa: F841
+        "and.b32 $0, $1, $2;",
+        "=r,r,r",
+        args=[input, other],
+        dtype=input.dtype,
+        is_pure=True,
+        pack=1,
+    )
+
+
+@functools.cache
+def make(ndim):
+    tensors = (Tensor(ndim), Tensor(ndim), Tensor(ndim))
+
+    return ninetoothed.make(arrangement, application, tensors)
