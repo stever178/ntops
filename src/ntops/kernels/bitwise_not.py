f6f3f2c040e24e8bb494caf61b09e7b54fa2f38c
Jiacheng Huang <huangjiacheng0709@outlook.com>
2025-05-30 14:30:21 +0800

    Replace inline assembly with native `~` operator

--- src/ntops/kernels/bitwise_not.py
+++ src/ntops/kernels/bitwise_not.py
@@ -1,21 +1,13 @@
 import functools
 
 import ninetoothed
-import ninetoothed.language as ntl
 from ninetoothed import Tensor
 
 from ntops.kernels.element_wise import arrangement
 
 
 def application(input, output):
-    output = ntl.inline_asm_elementwise(  # noqa: F841
-        "not.b32 $0, $1;",
-        "=r,r",
-        args=[input],
-        dtype=input.dtype,
-        is_pure=True,
-        pack=1,
-    )
+    output = ~input  # noqa: F841
 
 
 @functools.cache
