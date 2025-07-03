2043af6180edb50ca407d67edc864e55f6396748
Jiacheng Huang <huangjiacheng0709@outlook.com>
2025-07-03 16:21:30 +0800

    Support multi-dimensional reduction

--- src/ntops/kernels/reduction.py
+++ src/ntops/kernels/reduction.py
@@ -2,6 +2,11 @@ import ninetoothed
 
 
 def arrangement(*tensors, dim, block_size=None):
+    dims = dim
+
+    if isinstance(dims, int):
+        dims = (dims,)
+
     if block_size is None:
         block_size = ninetoothed.block_size()
 
@@ -9,19 +14,22 @@ def arrangement(*tensors, dim, block_size=None):
 
     assert all(tensor.ndim == ndim or tensor.ndim == 0 for tensor in tensors)
 
-    if dim < 0:
-        dim += ndim
+    dims = tuple(dim if dim >= 0 else dim + ndim for dim in dims)
 
-    inner_block_shape = tuple(1 if i != dim else block_size for i in range(ndim))
-    outer_block_shape = tuple(1 if i != dim else -1 for i in range(ndim))
-
-    non_target_dims = tuple(i for i in range(ndim) if i != dim)
+    non_target_dims = tuple(i for i in range(ndim) if i not in dims)
 
     def _arrange(tensor):
-        arranged = tensor.tile(inner_block_shape)
+        arranged = tensor.permute(non_target_dims + dims)
+        arranged = arranged.flatten(start_dim=-len(dims))
+
+        inner_block_shape = tuple(1 for _ in non_target_dims) + (block_size,)
+        outer_block_shape = tuple(1 for _ in non_target_dims) + (-1,)
+        non_target_dim_indices = tuple(range(len(non_target_dims)))
+
+        arranged = arranged.tile(inner_block_shape)
         arranged = arranged.tile(outer_block_shape)
-        arranged.dtype = arranged.dtype.squeeze(non_target_dims)
-        arranged.dtype.dtype = arranged.dtype.dtype.squeeze(non_target_dims)
+        arranged.dtype = arranged.dtype.squeeze(non_target_dim_indices)
+        arranged.dtype.dtype = arranged.dtype.dtype.squeeze(non_target_dim_indices)
 
         return arranged
 
